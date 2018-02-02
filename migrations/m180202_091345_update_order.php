e6eab63e3a27d71a7b46bff1295fa2073470eee8
tuanluong <luongtuan47@gmail.com>
2018-02-02 16:14:23 +0700

    commit

---
+++ migrations/m180202_091345_update_order.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180202_091345_update_order
+ */
+class m180202_091345_update_order extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('order', 'created_date', $this->timestamp()->defaultExpression( 'CURRENT_TIMESTAMP'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180202_091345_update_order cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180202_091345_update_order cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
