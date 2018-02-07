ce924a7f573e22339ec618bafb7cffd18631fee0
locpx <loc.xuanphama1t1@gmail.com>
2018-02-07 16:34:46 +0700

    phan quyen

--- widgets/views/sidebar.php
+++ widgets/views/sidebar.php
@@ -5,6 +5,18 @@
  * Date: 03/02/2017
  * Time: 11:12 AM
  */
+use app\controllers\ColorController;
+use app\controllers\CustomerController;
+use app\controllers\KpiEkipController;
+use app\controllers\KpiSaleController;
+use app\controllers\OrderController;
+use app\controllers\ProductController;
+use app\controllers\ReportController;
+use app\controllers\ScheduleAdvisoryController;
+use app\controllers\ServiceController;
+use app\controllers\TreatmentScheduleController;
+use navatech\role\controllers\DefaultController;
+use navatech\role\helpers\RoleChecker;
 use yii\widgets\Menu;
 
 ?>
@@ -13,13 +25,13 @@ use yii\widgets\Menu;
 echo Menu::widget([
 	'items'           => [
 		[
-			'label' => '<span class="nav-icon">
+			'label'   => '<span class="nav-icon">
                       <i class="material-icons">
                         <img style="width: 18px;" src="' . Yii::$app->request->baseUrl . '/images/message.png"/>
                       </i>
                     </span>',
-			'url'   => '#',
-			'items' => [
+			'url'     => '#',
+			'items'   => [
 				[
 					'label' => 'Quản lý nhân viên',
 					'url'   => ['/staff/index'],
@@ -29,6 +41,7 @@ echo Menu::widget([
 					'url'   => \yii\helpers\Url::to(['/role']),
 				],
 			],
+			'visible' => RoleChecker::isAuth(DefaultController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 		],
 		[
 			'label' => '<span class="nav-icon">
@@ -39,16 +52,19 @@ echo Menu::widget([
 			'url'   => '#',
 			'items' => [
 				[
-					'label' => 'Lịch hẹn tư vấn mới',
-					'url'   => ['/schedule-advisory/index'],
+					'label'   => 'Lịch hẹn tư vấn mới',
+					'url'     => ['/schedule-advisory/index'],
+					'visible' => RoleChecker::isAuth(ScheduleAdvisoryController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Danh sách khách hàng',
-					'url'   => ['/customer/index'],
+					'label'   => 'Danh sách khách hàng',
+					'url'     => ['/customer/index'],
+					'visible' => RoleChecker::isAuth(CustomerController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Tạo khách hàng',
-					'url'   => ['/customer/create'],
+					'label'   => 'Tạo khách hàng',
+					'url'     => ['/customer/create'],
+					'visible' => RoleChecker::isAuth(CustomerController::className(), 'create', Yii::$app->user->identity->getRoleId()),
 				],
 			],
 		],
@@ -61,16 +77,19 @@ echo Menu::widget([
 			'url'   => '#',
 			'items' => [
 				[
-					'label' => 'Quản lý dịch vụ',
-					'url'   => ['/service/index'],
+					'label'   => 'Quản lý dịch vụ',
+					'url'     => ['/service/index'],
+					'visible' => RoleChecker::isAuth(ServiceController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Quản lý sản phẩm',
-					'url'   => ['/product/index'],
+					'label'   => 'Quản lý sản phẩm',
+					'url'     => ['/product/index'],
+					'visible' => RoleChecker::isAuth(ProductController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Quản lý màu sắc',
-					'url'   => ['/color/index'],
+					'label'   => 'Quản lý màu sắc',
+					'url'     => ['/color/index'],
+					'visible' => RoleChecker::isAuth(ColorController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 			],
 		],
@@ -105,20 +124,24 @@ echo Menu::widget([
 			'url'   => '#',
 			'items' => [
 				[
-					'label' => 'Tạo đợn hàng',
-					'url'   => ['/order/create'],
+					'label'   => 'Tạo đợn hàng',
+					'url'     => ['/order/create'],
+					'visible' => RoleChecker::isAuth(OrderController::className(), 'create', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Quản lý đơn hàng',
-					'url'   => ['/order/index'],
+					'label'   => 'Quản lý đơn hàng',
+					'url'     => ['/order/index'],
+					'visible' => RoleChecker::isAuth(OrderController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Thêm lịch điều trị',
-					'url'   => ['/treatment-schedule/create'],
+					'label'   => 'Thêm lịch điều trị',
+					'url'     => ['/treatment-schedule/create'],
+					'visible' => RoleChecker::isAuth(TreatmentScheduleController::className(), 'create', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Quản lý lịch điều trị',
-					'url'   => ['/treatment-schedule/index'],
+					'label'   => 'Quản lý lịch điều trị',
+					'url'     => ['/treatment-schedule/index'],
+					'visible' => RoleChecker::isAuth(TreatmentScheduleController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 			],
 		],
@@ -131,8 +154,9 @@ echo Menu::widget([
 			'url'   => '#',
 			'items' => [
 				[
-					'label' => 'Doanh thu tổng quan',
-					'url'   => ['/report/index'],
+					'label'   => 'Doanh thu tổng quan',
+					'url'     => ['/report/index'],
+					'visible' => RoleChecker::isAuth(ReportController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 			],
 		],
@@ -145,12 +169,14 @@ echo Menu::widget([
 			'url'   => '#',
 			'items' => [
 				[
-					'label' => 'Kpi Sale',
-					'url'   => ['/kpi-sale/index'],
+					'label'   => 'Kpi Sale',
+					'url'     => ['/kpi-sale/index'],
+					'visible' => RoleChecker::isAuth(KpiSaleController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 				[
-					'label' => 'Kpi Ekip',
-					'url'   => ['/kpi-ekip/index'],
+					'label'   => 'Kpi Ekip',
+					'url'     => ['/kpi-ekip/index'],
+					'visible' => RoleChecker::isAuth(KpiEkipController::className(), 'index', Yii::$app->user->identity->getRoleId()),
 				],
 			],
 		],
