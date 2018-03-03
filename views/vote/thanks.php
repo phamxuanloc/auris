be9b259202a5ebe7be11e4950b4d1db2f0f423a7
locpx <loc.xuanphama1t1@gmail.com>
2018-03-03 18:03:14 +0700

    vote

--- views/vote/thanks.php
+++ views/vote/thanks.php
@@ -1,5 +1,4 @@
 <?php
-
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\helpers\Url;
@@ -10,11 +9,18 @@ use yii\helpers\Url;
 $this->title                   = 'Thanks';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="video-background">
-	<div class="video-foreground">
-		<iframe src="https://www.youtube.com/embed/rt6_XdkSYEw?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=rt6_XdkSYEw" frameborder="0" allowfullscreen></iframe>
-	</div>
+<!--<div class="video-background">-->
+<!--	<div class="video-foreground">-->
+<!--		<iframe src="https://www.youtube.com/embed/rt6_XdkSYEw?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=rt6_XdkSYEw" frameborder="0" allowfullscreen></iframe>-->
+<!--		<iframe width='500' height='294' src="https://www.youtube.com/embed/rt6_XdkSYEw?&theme=dark&autoplay=1&keyboard=1&autohide=1&playlist=rt6_XdkSYEw&iv_load_policy=3"  frameborder="0" class="youtube-player" type="text/html"></iframe>-->
+<div class="videoContainer">
+	<video loop autoplay id="myVideo">
+		<source src="uploads/Cover%20-%20Auris%20Dental%20-%20Viện%20Nha%20Khoa%20Thẩm%20Mỹ.mp4" type="video/mp4">
+		Your browser does not support the video tag.
+	</video>
 </div>
+<!--	</div>-->
+<!--</div>-->
 
 <!--<div id="vidtop-content">-->
 <!--	<div class="vid-info">-->
