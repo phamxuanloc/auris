6e7d0cb2f389b786c0258977465aa56ad5d9b25e
locpx <loc.xuanphama1t1@gmail.com>
2018-03-04 11:29:28 +0700

    fix video

--- views/vote/thanks.php
+++ views/vote/thanks.php
@@ -14,11 +14,11 @@ $this->params['breadcrumbs'][] = $this->title;
 <!--<iframe onload="loadImage()" src="https://www.youtube.com/embed/rt6_XdkSYEw" frameborder="0" allowfullscreen></iframe>-->
 <!--		<iframe width='500' height='294' src="https://www.youtube.com/embed/rt6_XdkSYEw?&theme=dark&autoplay=1&keyboard=1&autohide=1&playlist=rt6_XdkSYEw&iv_load_policy=3"  frameborder="0" class="youtube-player" type="text/html"></iframe>-->
 <!--<div class="videoContainer">-->
-<!--<video id="myVideo" autoplay muted loop playsinline>-->
-<!--	<source src="uploads/Cover%20-%20Auris%20Dental%20-%20Viện%20Nha%20Khoa%20Thẩm%20Mỹ.mp4">-->
-<!--	Your browser does not support the video tag.-->
-<!--</video>-->
-<img class="full-gif" src="uploads/test.gif">
+<video id="myVideo" autoplay muted loop>
+	<source src="uploads/Cover%20-%20Auris%20Dental%20-%20Viện%20Nha%20Khoa%20Thẩm%20Mỹ.mp4" type="video/mp4">
+	Your browser does not support the video tag.
+</video>
+<!--<img class="full-gif" src="uploads/test.gif">-->
 <!--<button onclick="abc()">play</button>-->
 <!--</div>-->
 <!--	</div>-->
@@ -30,13 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <!--	</div>-->
 <!--</div>-->
 <script>
-	var vid = document.getElementById("myVideo");
-	function abc() {
-		$('#myVideo').get(0).play()
-	}
-	//	$(document).ready(function() {
-	//		$("button").trigger("click");
-	//	});
+//	var vid = document.getElementById("myVideo");
 	var sse = new EventSource("<?=Url::to(['realtime'])?>");
 	sse.addEventListener('message', function(e) {
 		sse.close();
