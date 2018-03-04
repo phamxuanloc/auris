45a8a068cc44a5d2dfea311861261306d6421192
locpx <loc.xuanphama1t1@gmail.com>
2018-03-04 07:09:01 +0700

    fix video

--- views/vote/thanks.php
+++ views/vote/thanks.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <!--		<iframe width='500' height='294' src="https://www.youtube.com/embed/rt6_XdkSYEw?&theme=dark&autoplay=1&keyboard=1&autohide=1&playlist=rt6_XdkSYEw&iv_load_policy=3"  frameborder="0" class="youtube-player" type="text/html"></iframe>-->
 <div class="videoContainer">
 	<video loop autoplay id="myVideo">
-		<source src="uploads/Cover%20-%20Auris%20Dental%20-%20Viện%20Nha%20Khoa%20Thẩm%20Mỹ.mp4" type="video/mp4">
+		<source src="uploads/Cover%20-%20Auris%20Dental%20-%20Viện%20Nha%20Khoa%20Thẩm%20Mỹ.mp4">
 		Your browser does not support the video tag.
 	</video>
 </div>
