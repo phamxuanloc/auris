3ab07e419a527ce0d158fd78152b3cfb1087c302
locpx <loc.xuanphama1t1@gmail.com>
2018-03-04 07:33:27 +0700

    fix video

--- views/vote/thanks.php
+++ views/vote/thanks.php
@@ -11,14 +11,14 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <!--<div class="video-background">-->
 <!--	<div class="video-foreground">-->
-<!--		<iframe src="https://www.youtube.com/embed/rt6_XdkSYEw?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=rt6_XdkSYEw" frameborder="0" allowfullscreen></iframe>-->
+		<iframe src="https://www.youtube.com/embed/rt6_XdkSYEw" frameborder="0" allowfullscreen></iframe>
 <!--		<iframe width='500' height='294' src="https://www.youtube.com/embed/rt6_XdkSYEw?&theme=dark&autoplay=1&keyboard=1&autohide=1&playlist=rt6_XdkSYEw&iv_load_policy=3"  frameborder="0" class="youtube-player" type="text/html"></iframe>-->
-<div class="videoContainer">
-	<video id="myVideo">
-		<source src="uploads/Cover%20-%20Auris%20Dental%20-%20Viện%20Nha%20Khoa%20Thẩm%20Mỹ.mp4">
-		Your browser does not support the video tag.
-	</video>
-</div>
+<!--<div class="videoContainer">-->
+<!--	<video id="myVideo">-->
+<!--		<source src="uploads/Cover%20-%20Auris%20Dental%20-%20Viện%20Nha%20Khoa%20Thẩm%20Mỹ.mp4">-->
+<!--		Your browser does not support the video tag.-->
+<!--	</video>-->
+<!--</div>-->
 <!--	</div>-->
 <!--</div>-->
 
