41808cc5e4f63f7db85cae0a1927d6ae2dcb5c70
Loc Pham <loc.xuanphama1t1@gmail.com>
2018-11-24 14:45:05 +0700

    add media customer

--- migrations/m181123_223611_order_service.php
+++ migrations/m181123_223611_order_service.php
@@ -23,11 +23,6 @@ class m181123_223611_order_service extends Migration
                 ],
             $tableOptions
         );
-    $this->insert('{{%order_service}}',['id'=>'1','order_id'=>'33','service_id'=>'6','product_id'=>'6','color_id'=>'1','price'=>'4000000','quantity'=>'2','total_price'=>'8000000']);
-$this->insert('{{%order_service}}',['id'=>'2','order_id'=>'34','service_id'=>'6','product_id'=>'4','color_id'=>'','price'=>'4000000','quantity'=>'1','total_price'=>'4000000']);
-$this->insert('{{%order_service}}',['id'=>'3','order_id'=>'34','service_id'=>'7','product_id'=>'5','color_id'=>'1','price'=>'1000000','quantity'=>'2','total_price'=>'2000000']);
-$this->insert('{{%order_service}}',['id'=>'4','order_id'=>'34','service_id'=>'6','product_id'=>'4','color_id'=>'','price'=>'4000000','quantity'=>'1','total_price'=>'4000000']);
-
     }
 
     public function safeDown()
